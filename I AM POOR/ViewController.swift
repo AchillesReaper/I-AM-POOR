a0bba0c0e5c4f2eea3698a7348e73d5fc0ca4b48
Donald Ho <Donald.h.ho@gmail.com>
2022-08-21 16:00:43 +1000

    Initial Commit

---
+++ I AM POOR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Donald Ho on 21/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
